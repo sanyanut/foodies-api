070c6e6853aa21c1d4a43e0aa2d69fc2e5aab44d
Tanya <tanya.chapaliuk@gmail.com>
2026-08-19 22:56:07 +0300

    feat(categories): sort categories alphabetically by name

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,5 +1,4 @@
 import prisma from "../../prisma/prisma.ts";
 
-export const getAllCategories = async () => {
-  return await prisma.category.findMany();
-};
+export const getAllCategories = () =>
+  prisma.category.findMany({ orderBy: { name: "asc" } });
