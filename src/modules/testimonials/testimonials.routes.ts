d9c34bf3d89ba678fb0a441acf63b9e72646ac79
Inna Khomovska <innapariy@gmail.com>
2026-08-19 21:36:47 +0200

    docs: add swagger documentation for testimonials endpoint

--- src/modules/testimonials/testimonials.routes.ts
+++ src/modules/testimonials/testimonials.routes.ts
@@ -1,8 +1,34 @@
-import {Router} from 'express';
-import {getTestimonials} from './testimonials.controller.ts';
+import { Router } from "express";
+import { getTestimonials } from "./testimonials.controller.ts";
 
 const router = Router();
+/**
+ * @openapi
+ * /testimonials:
+ *   get:
+ *     summary: Get all testimonials
+ *     tags:
+ *       - Testimonials
+ *     responses:
+ *       200:
+ *         description: List of all testimonials
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                   testimonial:
+ *                     type: string
+ *                   ownerId:
+ *                     type: string
+ *                   createdAt:
+ *                     type: string
+ *                     format: date-time
+ */
+router.get("/", getTestimonials);
 
-router.get('/', getTestimonials);
-
-export default router;  
+export default router;
